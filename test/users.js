582ee094200d0a6e036d8c5c63e679ae9b419027
nizam754 <m.nizam754@outlook.com>
2021-03-10 20:40:25 +0600

    API Tests for HTTP POST method

--- test/users.js
+++ test/users.js
@@ -1,38 +1,58 @@
-import supertest from 'supertest';
-const request = supertest('https://gorest.co.in/public-api/');
+import supertest from "supertest";
+const request = supertest("https://gorest.co.in/public-api/");
 
-import {expect} from 'chai';
+import { expect } from "chai";
 
-const TOKEN = 'c02177b60339dcc1d981da685bdfd5a6546606e03d3c158b99d2d686c7fd4e40';
+const TOKEN =
+  "c02177b60339dcc1d981da685bdfd5a6546606e03d3c158b99d2d686c7fd4e40";
 
-describe('Users', () => {
-    it('GET /users', () => {
-        /* request.get(`users?access-token=${TOKEN}`).end((err, res) => {
+describe("Users", () => {
+  it("GET /users", () => {
+    /* request.get(`users?access-token=${TOKEN}`).end((err, res) => {
             //console.log(err);
             //console.log(res.body);
             expect(res.body.data).to.not.be.empty;
             done();
         }); */
-        return request.get(`users?access-token=${TOKEN}`).then((res) => {
-            expect(res.body.data).to.not.be.empty;
-        });
+    return request.get(`users?access-token=${TOKEN}`).then((res) => {
+      expect(res.body.data).to.not.be.empty;
     });
+  });
 
-    it('GET /users/:id', () => {
-        return request.get(`users/1?access-token=${TOKEN}`).then((res) => {
-            expect(res.body.data.id).to.not.be.eq(1);
-        });
+  it("GET /users/:id", () => {
+    return request.get(`users/1?access-token=${TOKEN}`).then((res) => {
+      expect(res.body.data.id).to.not.be.eq(1);
     });
+  });
 
-    it('GET /users with query params', () => {
-        const url = `users?access-token=${TOKEN}&page=5&gender=Female&status=Active`
-        
-        return request.get(url).then((res) => {
-            expect(res.body.data).to.not.be.empty;
-            res.body.data.forEach(data => {
-                expect(data.gender).to.eq('Female');
-                expect(data.status).to.eq('Active');
-            })
-        });
+  it("GET /users with query params", () => {
+    const url = `users?access-token=${TOKEN}&page=5&gender=Female&status=Active`;
+
+    return request.get(url).then((res) => {
+      expect(res.body.data).to.not.be.empty;
+      res.body.data.forEach((data) => {
+        expect(data.gender).to.eq("Female");
+        expect(data.status).to.eq("Active");
+      });
     });
+  });
+
+  it.only("POST /users", () => {
+    const data = {
+      email: `test-${Math.floor(Math.random() * 9999)}@mail.com`,
+      name: "test name",
+      gender: "Male",
+      status: "Inactive",
+    };
+
+    return request
+      .post("users")
+      .set("Authorization", `Bearer ${TOKEN}`)
+      .send(data)
+      .then((res) => {
+        // console.log(res.body);
+        // data.gender = "Female";
+        expect(res.body.data).to.deep.include(data);
+      });
+  });
 });
